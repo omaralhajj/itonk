bc42dacc58328a6aa0e0f50723acee4323733fc5
Sujeeth <sujeeth.nalliah@gmail.com>
2019-03-04 20:44:16 +0100

    tilføjet backend. Der er problemer med connection string

---
+++ Backend/delopgave/delopgave/Models/Haandvaerker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace delopgave.Models
+{
+    public class Haandvaerker
+    {
+        public string HvAnsaettelsedatao { get; set; }
+        public string HvEfternavn { get; set; }
+        public string HvFagomraade { get; set; }
+        public string HvFornavn { get; set; }
+        [Key]
+        public int HaandvaerkerId { get; set; }
+        public ICollection<Vaerktoejskasse> Vaerker { get; protected set; }
+
+        public Haandvaerker()
+        {
+            Vaerker = new List<Vaerktoejskasse>();
+        }
+    }
+}

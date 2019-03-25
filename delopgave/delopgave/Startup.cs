47d64ca5557253a4759aa76b3ec9a5864e85bb59
Omar <omar-alhajj@hotmail.com>
2019-03-25 16:22:13 +0100

    virker næsten lige på nær frontend

--- delopgave/delopgave/Startup.cs
+++ delopgave/delopgave/Startup.cs
@@ -35,8 +35,9 @@ namespace delopgave
             services.AddHttpClient();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            var host = "gr8mssqlserver";
             services.AddDbContext<delopgaveContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("delopgaveContext")));
+                    options.UseSqlServer("Data Source=" + host + "; User ID=SA; Password=F19ItOnk; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- delopgave/frontend/Startup.cs
+++ delopgave/frontend/Startup.cs
@@ -24,7 +24,7 @@ namespace frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var host = Configuration["HOST"];
+            var host = "gr8delopgavebackend";
 
             services.Configure<CookiePolicyOptions>(options =>
             {

d2ea3fff9400277d5e8a4936fcd9fbd2ef86f202
sn <sujeeth.nalliah@gmail.com>
2019-04-01 12:41:38 +0200

    Added StockBroker Models

---
+++ Gr8TSEIS/StockBroker/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockBroker.Models
+{
+    public class Transaction
+    {
+        public Guid ID { get; set; }
+        public Guid BuyerID { get; set; }
+        public Guid ShareID { get; set; }
+        public Guid SellerID { get; set; }
+        public decimal MoneyValue { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}

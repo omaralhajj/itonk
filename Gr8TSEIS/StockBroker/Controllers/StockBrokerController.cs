d985393176cd51ba114e03ddafe2d6c3cd1acd7f
sn <sujeeth.nalliah@gmail.com>
2019-04-01 13:07:29 +0200

    Added StockBrokerController.cs

--- Gr8TSEIS/StockBroker/Controllers/StockBrokerController.cs
+++ Gr8TSEIS/StockBroker/Controllers/StockBrokerController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StockBroker.Models;
 
 namespace StockBroker.Controllers
 {
     public class StockBrokerController: Controller
     {
+        [HttpPut()]
+        public async Task<IActionResult> UpdateShare(Guid TraderID, Guid ShareID)
+        {
+            var tid = TraderID;
+            var sid = ShareID;
+
+
+
+            return await x;
+        }
 
     }
 }

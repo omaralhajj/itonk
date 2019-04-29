70c0cbf61b8d14107f71eb16069d67ec7116d3e3
sn <sujeeth.nalliah@gmail.com>
2019-04-29 10:31:24 +0200

    Added microservices classes

--- Gr8TSEIS/StockBroker/Controllers/StockBrokerController.cs
+++ Gr8TSEIS/StockBroker/Controllers/StockBrokerController.cs
@@ -1,24 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
 using StockBroker.Models;
 
 namespace StockBroker.Controllers
 {
-    public class StockBrokerController: Controller
+    public class StockBrokerController : Controller
     {
+        public HttpClient HttpClient { get; }
+
         [HttpPut()]
-        public async Task<IActionResult> UpdateShare(Guid TraderID, Guid ShareID)
+        public async Task<Share> UpdateShare(Guid TraderID, Guid ShareID)
         {
             var tid = TraderID;
             var sid = ShareID;
 
+            var result = new Share();
 
+            foreach (var partition in partitions)
+            {
+             
+       
+                var httpContent = JsonConvert.SerializeObject(share);
+                var byteContent = new ByteArrayContent(System.Text.Encoding.UTF8.GetBytes(httpContent));
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-            return await x;
+                using (var response = await HttpClient.PutAsync(proxyUrl, byteContent))
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        throw new StockBrokerException((HttpErrorCode) response.StatusCode, response.ReasonPhrase);
+                    }
+
+                    result = JsonConvert.DeserializeObject<Share>(
+                        await response.Content.ReadAsStringAsync()
+                    );
+                }
+            }
+            return result;
         }
-
     }
 }
